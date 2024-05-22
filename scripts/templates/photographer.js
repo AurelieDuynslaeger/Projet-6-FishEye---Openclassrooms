5eec15e9a0bd808ea5172fc09b644c87a03532fa
AureLeeLi <aduynslaeger@gmail.com>
2024-05-22 23:17:54 +0200

    ⚡️ fetch photographers ok + 🔥 ajout de la balise a pour l'élement clikable vers le profil photographe + ♿️  ajout alt photographer name

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -2,16 +2,22 @@ function photographerTemplate(data) {
     const { name, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const link = `link to ${name} photographer profile`
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        //ajout de l'élément clickable vers le profile du photographe
+        const profileLink = document.createElement('a');
+        //définition du texte alternatif pour le lien
+        profileLink.setAttribute("alt", link);
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        profileLink.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        return (profileLink);
     }
     return { name, picture, getUserCardDOM }
 }
