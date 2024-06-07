42f194534487660527bf6f1d57709157357928c4
AureLeeLi <aduynslaeger@gmail.com>
2024-06-07 19:09:17 +0200

    ✅ add bannerPhotographer template

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,6 +3,7 @@ import { getPhotographers, getMedias } from '../utils/data.js';
 import { handleSortChange } from '../utils/sort.js';
 import MediasFactory from "../factories/MediasFactory.js";
 import { getTotalLikes } from "../templates/photographer.js"
+import { bannerPhotographer } from "../templates/bannerPhotographer.js";
 
 // Variable pour stocker les informations du photographe
 let currentPhotographer = null;
@@ -25,12 +26,14 @@ document.addEventListener("DOMContentLoaded", async () => {
         //si le photographe est trouvé
         if (photographer) {
             console.log(photographer);
-            currentPhotographer = photographer; // Stocke les infos du photographe
-            displayPhotographerData(photographer);
+            currentPhotographer = photographer;
+            bannerPhotographer(photographer);
             const { medias } = await getMedias();
             const photographerMedias = medias.filter(media => media.photographerId == idPhotograh);
             console.log(photographerMedias);
             //on display ses infos comme avec displayData
+
+            //le for each serait ici ?? 
             displayPhotographerMedia(photographer, photographerMedias);
         } else {
             console.error("Photographe inconnu")
@@ -39,43 +42,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 
-//affichage des données des photographe home page
-function displayPhotographerData(photographer) {
-
-    //on cible les sections de photographer.html
-    const header = document.querySelector('.photograph-header');
-
-    //h1 qui display le nom
-    const h1 = document.createElement('h1');
-    h1.textContent = photographer.name;
-
-    //h3 qui display la ville
-    const location = document.createElement('h2');
-    location.textContent = `${photographer.city}, ${photographer.country}`;
-
-    //p qui display la tagline
-    const tagline = document.createElement('p');
-    tagline.textContent = photographer.tagline;
-
-
-    //div .photographer-info qui regroupe nom, ville, tagline
-    const identityInfo = document.createElement('div');
-    identityInfo.classList.add("photographer-info");
-    identityInfo.appendChild(h1);
-    identityInfo.appendChild(location);
-    identityInfo.appendChild(tagline);
-
-    //on insère cette div dans le header avant le boutton de contact
-    header.insertBefore(identityInfo, header.querySelector('.contact_button'));
-
-    //on display la photo de profile avec la src et le alt text pour le nom du photograph
-    const picture = document.createElement('img');
-    picture.setAttribute("src", `assets/photographers/${photographer.portrait}`);
-    picture.setAttribute('alt', photographer.name);
-
-    // header.appendChild(identityInfo);
-    header.appendChild(picture);
-}
 
 //contenu affiché selon le tri ou non
 let currentMedias = [];
---
+++ scripts/templates/mediaItemPhotographer.js
@@ -0,0 +1,3 @@
+export function mediaItemPhotographer(media) {
+
+}
